b531c88be1ef78d27afbfc1c00f84b06569559dc
Chibueze Anakor <chibuezeanakor@users.noreply.github.com>
2019-06-12 00:45:17 -0400

    initial commit for heroku

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { StaticRouter } from 'react-router-dom';
 import './index.css';
 import App from './Components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<StaticRouter basename="/jammming"><App /></StaticRouter>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
